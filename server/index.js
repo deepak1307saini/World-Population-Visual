46ab9eb2f0ff33429d62a28d6740b18a904f9fd5
Deepak Saini <deepak1307saini@gmail.com>
2023-10-05 18:41:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,12 +6,6 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors({
-    origin: "https://frontend-taupe-six-23.vercel.app",
-    methods: ["POST", "GET"],
-    credentials: true 
-}));
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
