bf287faf04d2d627f5eda911b0b6ba9079d91361
Deepak Saini <deepak1307saini@gmail.com>
2023-10-05 18:31:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors({
-    origin: "https://deploy.mern-1whq.vercel.app",
+    origin: "https://frontend-taupe-six-23.vercel.app/",
     methods: ["POST", "GET"],
     credentials: true 
 }));
